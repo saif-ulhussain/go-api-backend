49a9be48c48be37451655ae90aea2648667d6a67
saif-ulhussain <saif-ul.hussain@sainsburys.co.uk>
2023-07-20 23:29:16 +0100

    Rectified create habit test

--- internal/handlers/create_habit_test.go
+++ internal/handlers/create_habit_test.go
@@ -2,12 +2,10 @@ package handlers_test
 
 import (
 	"bytes"
-	"encoding/json"
 	"github.com/golang/mock/gomock"
 	. "github.com/onsi/ginkgo"
 	"github.com/onsi/gomega"
 	"go-api-backend/internal/handlers"
-	"go-api-backend/internal/models"
 	"go-api-backend/mocks"
 	"net/http"
 	"net/http/httptest"
@@ -15,31 +13,31 @@ import (
 
 var _ = Describe("CreateHabitHandler", func() {
 
+	var habitHandler *handlers.HabitHandler
+	var mockRepo *mocks.MockHabitRepositoryInterface
+
+	BeforeEach(func() {
+		ctrl := gomock.NewController(GinkgoT())
+		defer ctrl.Finish()
+		mockRepo = mocks.NewMockHabitRepositoryInterface(ctrl)
+		habitHandler = handlers.NewHabitHandler(mockRepo)
+	})
+
 	Context("When valid habit data is provided", func() {
 		It("Should return a status response of 201", func() {
+			validJSON := []byte(`{
+				"name": "Complete Leet Code Exercise...",
+				"start_date": "2012-01-02",
+				"end_date": null,
+				"streak_count": 20,
+				"completed": true,
+				"comments": "great!",
+				"category": "Golang"
+			}`)
 
-			ctrl := gomock.NewController(GinkgoT())
-			defer ctrl.Finish()
+			mockRepo.EXPECT().InsertHabit(gomock.Any()).Return(nil)
 
-			mockRepo := mocks.NewMockHabitRepositoryInterface(ctrl)
-			habitHandler := handlers.NewHabitHandler(mockRepo)
-
-			habit := models.Habit{
-				Name:        "Exercise 3",
-				StartDate:   "2012-01-02",
-				EndDate:     nil,
-				StreakCount: nil,
-				Completed:   nil,
-				Comments:    nil,
-				Category:    nil,
-			}
-
-			mockRepo.EXPECT().InsertHabit(gomock.AssignableToTypeOf(habit)).Return(nil)
-
-			habitJSON, err := json.Marshal(habit)
-			gomega.Expect(err).NotTo(gomega.HaveOccurred())
-
-			req, err := http.NewRequest(http.MethodPost, "/habits", bytes.NewBuffer(habitJSON))
+			req, err := http.NewRequest(http.MethodPost, "/habits", bytes.NewBuffer(validJSON))
 			gomega.Expect(err).NotTo(gomega.HaveOccurred())
 
 			res := httptest.NewRecorder()
@@ -48,6 +46,31 @@ var _ = Describe("CreateHabitHandler", func() {
 
 			gomega.Expect(res.Code).To(gomega.Equal(http.StatusCreated))
 			gomega.Expect(res.Body.String()).To(gomega.Equal("Entry successfully created."))
+		})
+	})
+
+	Context("When incomplete habit data is provided / mandatory property is missing", func() {
+		It("Should return a status response of 400", func() {
+			invalidJSON := []byte(`{
+            "name": "Complete Leet Code Exercise...",
+            "start_date": null,
+            "end_date": null,
+            "streak_count": "IncorrectDataType"",
+            "completed": true,
+            "comments": "great!",
+            "category": "Golang" }`)
+
+			req, err := http.NewRequest(http.MethodPost, "/habits", bytes.NewBuffer(invalidJSON))
+			gomega.Expect(err).NotTo(gomega.HaveOccurred())
+
+			res := httptest.NewRecorder()
+
+			mockRepo.EXPECT().InsertHabit(gomock.Any()).Return(nil)
+
+			habitHandler.CreateHabitHandler(res, req)
+
+			gomega.Expect(res.Code).To(gomega.Equal(http.StatusBadRequest))
+			gomega.Expect(res.Body.String()).To(gomega.ContainSubstring("Invalid request body"))
 		})
 	})
 })
